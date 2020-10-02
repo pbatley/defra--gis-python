62ae2b5780d8a57e7c68b78585bc2d5437a8f2da
pbatley <paul.batley@landmark.co.uk>
2020-10-02 16:10:08 +0100

    Update loadConfig.py

--- UserManager/config/loadConfig.py
+++ UserManager/config/loadConfig.py
@@ -1,7 +1,9 @@
 
-envURL="https://environment-test.data.gov.uk/portal"
+envURL="https://environment-test.data.gov.uk/"
+suffixPub = "portal"
+suffixNonPub = "portalstg"
 portalOwner = "portaladmin"
-portalPWD = "ArcG!$3nt3rpr!$3Adm!n"
+portalPWD = "AGS3nt3rpr!$3Adm!n"
 userSearch = None
 groupSearch = None
 groupSearchWithMembers = None
@@ -10,3 +12,28 @@ grpids = 'Service Owner'
 uids = None#{}
 #uids = None#'test'
 email = 'test.com'
+
+TST={'envURL':'https://environment-test.data.gov.uk/',
+    'suffixPub':'portal',
+    'portalOwner':'portaladmin',
+    'portalPWD':'AGS3nt3rpr!$3Adm!n',
+    'userSearch':None,
+    'groupSearch':None,
+    'groupSearchWithMembers':None,
+    'dataFile':'\\data\\test-dlsm.csv',
+    'grpids':'Service Owner',
+    'uids':None,
+    'email':'test.com'
+  }
+UAT={'envURL':'https://environment-uat.data.gov.uk/',
+    'suffixPub':'portal',
+    'portalOwner':'portaladmin',
+    'portalPWD':'AGS3nt3rpr!$3Adm!n',
+    'userSearch':None,
+    'groupSearch':None,
+    'groupSearchWithMembers':None,
+    'dataFile':'\\data\\test-dlsm.csv',
+    'grpids':'Service Owner',
+    'uids':None,
+    'email':'test.com'
+  }
